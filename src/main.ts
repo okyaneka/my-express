32062c10c918858a4e4f67ad2378ecc47a557e22
Oky Aneka <okyaneka@gmail.com>
2024-09-21 12:08:35 +0700

    test: github action

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
